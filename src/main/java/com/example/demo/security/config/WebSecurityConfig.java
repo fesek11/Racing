2945be15cda8e6dec999a1a44756eb8b92fd6f71
fesek11 <dfesek1@gmail.com>
2022-09-15 20:57:59 +0300

    Do spring web security

--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -29,8 +30,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
                 .antMatchers("/api/v1/participant/participants").hasRole("USER")
                 .antMatchers("/api/v1/racing/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/api/v1/registration").permitAll()
